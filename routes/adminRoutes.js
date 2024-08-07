62c4ea253bb40d3665a0102a5d5301291ff18b40
AllanMusinguzi <pmusinguzi69@yahoo.com>
2024-08-07 08:34:13 +0300

    Modified a few files

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -17,13 +17,13 @@ router.get("/", adminController.redirectToLogin);
 router.get("/login", adminController.getLoginPage);
 router.post("/login_process", adminController.loginAdmin);
 router.get("/dashboard", authMiddleware.isAdminLoggedIn, adminController.getAdminDashboard);
-router.get("/all-requests", authMiddleware.isAdminLoggedIn, adminController.getAllRequests);
+router.get("/allRequests", authMiddleware.isAdminLoggedIn, adminController.getAllRequests);
 router.get("/assign-driver", authMiddleware.isAdminLoggedIn, requestMiddleware.isRequestRejected, adminController.assignDriver);
 router.get("/unassign-driver", authMiddleware.isAdminLoggedIn, requestMiddleware.isRequestRejected, adminController.unassignDriver);
 router.get("/reject-request", authMiddleware.isAdminLoggedIn, requestMiddleware.isRequestRejected, adminController.rejectRequest);
 router.get("/create-driver", authMiddleware.isAdminLoggedIn, adminController.getCreateDriverPage);
 router.post("/create-driver", authMiddleware.isAdminLoggedIn, adminController.createDriver);
-router.get("/all-drivers", authMiddleware.isAdminLoggedIn, adminController.getAllDrivers);
+router.get("/allDrivers", authMiddleware.isAdminLoggedIn, adminController.getAllDrivers);
 router.get("/delete-driver", authMiddleware.isAdminLoggedIn, adminController.deleteDriver);
 router.get("/logout", adminController.logoutAdmin);
 
